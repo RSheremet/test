5bbe12a902f32cd17dbe8406a0c1ef55ddf67a27
RSheremet <roma.sheremet.91@mail.ru>
2020-04-17 19:39:06 +0400

    Updates

---
+++ precache-manifest.f3a877479919fddd700039de50ce2299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ad19b58cc866235a77db62c5dd364b",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b700b62de65ff651198f",
+    "url": "/test/static/css/main.88540d09.chunk.css"
+  },
+  {
+    "revision": "41343c81f98869af9120",
+    "url": "/test/static/js/2.4d27bd66.chunk.js"
+  },
+  {
+    "revision": "b700b62de65ff651198f",
+    "url": "/test/static/js/main.d3517e3d.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
